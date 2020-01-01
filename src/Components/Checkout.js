92a4e9db40473023aa19bd8e1d5bc89a52ff58d5
arulkhair <amashdarulk@gmail.com>
2020-01-01 23:54:23 +0700

    add checkout function and print to pdf

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Modal, Button, Row, Col, Input } from "antd";
 import { Typography } from "antd";
-
+import Axios from "axios";
+import Swal from "sweetalert2";
+import jsPDF from "jspdf";
 const { Title } = Typography;
 class Checkout extends React.Component {
-  state = {
-    loading: false,
-    visible: false
-  };
+  constructor(props) {
+    super(props);
+    this.handleChangeCashier = this.handleChangeCashier.bind(this);
+    this.handleCheckout = this.handleCheckout.bind(this);
+    this.state = {
+      loading: false,
+      visible: false,
+      cashier: ""
+    };
+  }
 
   showModal = () => {
     this.setState({
@@ -23,8 +31,106 @@ class Checkout extends React.Component {
   };
 
   handleCancel = () => {
-    this.setState({ visible: false });
+    this.setState({ visible: false, handlingInput: "" });
   };
+
+  formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  }
+
+  handleCheckout() {
+    let codeReceipt = this.props.codeR;
+    let cashier = this.state.cashier;
+    let checkout = this.props.dataCheckout;
+    let ppn = this.props.total * 0.1;
+    let total = this.props.total + ppn;
+    // let created_at = new Date();
+    let created_at = this.props.date;
+    if (this.state.cashier === "")
+      this.setState({
+        handlingInput: "* Cashier Cannot Empty"
+      });
+    else {
+      console.log("receipt code:", codeReceipt);
+      console.log("cashier:", cashier);
+      console.log("ppn:", ppn);
+      console.log("total:", total);
+      console.log("created_at:", created_at);
+      // console.log("checkout item:", checkout);
+      const transactionNew = {
+        transaction_code: codeReceipt,
+        cashier: cashier,
+        total: total,
+        ppn: ppn,
+        created_at: created_at
+      };
+
+      this.setState({ loading: true });
+      setTimeout(() => {
+        this.setState({ loading: false, visible: false });
+        Axios.post("http://localhost:6660/api/transaction", transactionNew)
+          .then(() => {
+            if (checkout.length > 0) {
+              return checkout.map(item => {
+                let transactionMenu = {
+                  transaction_code: codeReceipt,
+                  id_menu: item.id,
+                  quantity: item.count,
+                  price: item.price,
+                  created_at: created_at
+                };
+                Axios.post(
+                  "http://localhost:6660/api/transaction/menu",
+                  transactionMenu
+                );
+              });
+            }
+          })
+          .then(() => {
+            Swal.fire(
+              "Checkout Success",
+              "Transaction has been finished",
+              "success"
+            ).then(() => {
+              document.location.href = "/";
+              var doc = new jsPDF();
+              let space = 10;
+              doc.text(`Receipt Code ${codeReceipt}`, 10, (space += 10));
+              doc.text(`Cashier ${cashier}`, 10, (space += 10));
+              // doc.text(`Cashier ${cashier}`, 10, 30);
+              // if (checkout.length > 0) {
+              checkout.map(items => {
+                // doc.text(`${items.name} ${items.count}x Rp. ${this.formatNumber(items)}`, 10, 10);
+                doc.text(
+                  `${items.name} ${items.count}x Rp. ${this.formatNumber(
+                    items.price
+                  )}`,
+                  10,
+                  (space += 10)
+                );
+              });
+              // }
+              doc.text(
+                `Total Rp. ${this.formatNumber(total)}`,
+                10,
+                (space += 10)
+              );
+              doc.text(`Payment Cash`, 10, (space += 10));
+              doc.save(`${codeReceipt}.pdf`);
+            });
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }, 3000);
+    }
+  }
+
+  handleChangeCashier(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
   formatNumber(num) {
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
@@ -54,33 +160,44 @@ class Checkout extends React.Component {
               key="submit"
               type="primary"
               loading={loading}
-              onClick={this.handleOk}
+              onClick={this.handleCheckout}
             >
               Print
-            </Button>
+            </Button>,
+            <p style={{ fontSize: 13, color: "red", float: "left" }}>
+              {this.state.handlingInput}
+            </p>
           ]}
         >
           <center>
             <Title level={4}>Receipt code. {codeReceiptString} </Title>
-            <Input placeholder="Chasier" style={{ width: "62%" }} />
+            <Input
+              value={this.state.cashier}
+              name="cashier"
+              onChange={this.handleChangeCashier}
+              placeholder="Chasier"
+              style={{ width: "62%" }}
+            />
           </center>
-          {checkout.map((item, index) => {
-            return (
-              <Row>
-                <Col span={16}>
-                  <Title level={3}>
-                    {item.name} {item.count}x
-                  </Title>
-                </Col>
-                <Col span={8}>
-                  <Title level={3}>
-                    Rp. {this.formatNumber(item.price * item.count)}
-                  </Title>
-                </Col>
-              </Row>
-              // <Title level={4}>Receipt code. {codeReceiptString} </Title>
-            );
-          })}
+          {checkout
+            .map((item, index) => {
+              return (
+                <Row>
+                  <Col span={16}>
+                    <Title level={3}>
+                      {item.name} {item.count}x
+                    </Title>
+                  </Col>
+                  <Col span={8}>
+                    <Title level={3}>
+                      Rp. {this.formatNumber(item.price * item.count)}
+                    </Title>
+                  </Col>
+                </Row>
+                // <Title level={4}>Receipt code. {codeReceiptString} </Title>
+              );
+            })
+            .reverse()}
           <Row>
             <Col span={16}>
               <Title level={3}>Ppn 10%</Title>
